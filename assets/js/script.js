3ac1ceb2f467f0aa28ec071c15a810e973bb78ad
v3sker <vespucci.samp@gmail.com>
2024-05-30 20:33:01 +0300

    feat: update of gui
    Changelog:
    - Implemented controls. Now game is running just by using 3 buttons: rock, paper, and.. surprise.. scissors.
    - Refactor of the game logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,72 @@
+const roundCounterText = document.querySelector('#round-counter')
+
+const statsContainer = document.querySelector('.game-stats')
+const statsHeader = document.createElement('h3');
+const statsPara = document.createElement('p');
+
+const btnRock = document.querySelector('#btn-rock');
+const btnPaper = document.querySelector('#btn-paper');
+const btnSciss = document.querySelector('#btn-scis');
+
 let playerScore = 0;
 let computerScore = 0;
-let currentRound = 0;
-const ROUND_COUNTER = document.querySelector("#round-counter");
-const PLAYER_SCORE_TEXT = document.querySelector('#player-score');
-const COMPUTER_SCORE_TEXT = document.querySelector('#computer-score');
+let roundCounter = 0;
 
-// Initializing round counter on site to not be blank on load.
-ROUND_COUNTER.innerHTML = currentRound;
-
-// Random based computer choice between rock, paper and scissors
-function getComputerChoice(){
-  let randomizer = Math.floor(Math.random() * 3);
-  return randomizer == 0 ? "rock"
-    : randomizer == 1 ? "paper"
-    : "scissors";
+function updateStats() {
+  roundCounterText.textContent = roundCounter;
 }
 
-// Prompt based player choice
-function getPlayerChoice(){
-  let input = prompt("Choose your weapon (rock, paper or scissors)");
-  if (input == null){
-    while (input == null){
-      input = prompt("Choose your weapon (rock, paper or scissors)");
-    }
-  } else {input.toLowerCase}
-  return input == 'rock' || input == 'paper' || input == 'scissors' ? input
-    : "Input error! Try again."
+updateStats();
+
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3)
+  return (random === 0) ? 'rock' 
+    : (random === 1) ? 'paper' 
+    : (random === 2) ? 'scissors'
+    : 'ERROR';
 }
 
-// Game round algorithm (yea, hardcoded, what did you expect? It's freakin rock paper scissors.)
-function playRound(playerChoice, computerChoice){
+function winner(playerChoice, computerChoice) {
   return playerChoice == 'rock' && computerChoice == 'scissors' ? 'player win'
     : playerChoice == 'paper' && computerChoice == 'rock' ? 'player win'
     : playerChoice == 'scissors' && computerChoice == 'paper' ? 'player win'
+
     : playerChoice == 'rock' && computerChoice == 'rock' ? 'draw'
     : playerChoice == 'paper' && computerChoice == 'paper' ? 'draw'
     : playerChoice == 'scissors' && computerChoice == 'scissors' ? 'draw'
+
     : playerChoice == 'rock' && computerChoice == 'paper' ? 'computer win'
     : playerChoice == 'paper' && computerChoice == 'scissors' ? 'computer win'
     : playerChoice == 'scissors' && computerChoice == 'rock' ? 'computer win'
-    : "Error. Try reloading the page.";
+    : "ERROR";
 }
 
-// Game loop function, looping 5 "rounds"
-function playGame(){
-  for(let i = 0; i < 5; i++){
-    let winner = playRound(getPlayerChoice(), getComputerChoice());
-    if (winner == 'player win'){
-      playerScore++;
-      console.log(`Player won! Score: player - ${playerScore}, computer - ${computerScore}`);
-    } else if (winner == 'computer win') {
-      computerScore++;
-      console.log(`Computer won! Score: player - ${playerScore}, computer - ${computerScore}`);
-    } else {console.log('Draw!')}
-    currentRound++;
-    ROUND_COUNTER.innerHTML = currentRound;
+function playRound(playerChoice){
+  let computerChoice = getComputerChoice();
+  let roundWinner = winner(playerChoice, computerChoice);
+
+  if (roundWinner === 'player win') {
+    statsHeader.textContent = 'Round won!';
+    statsPara.textContent = `Computer choice: ${computerChoice}.`;
+    playerScore++;
+    roundCounter++;
+    updateStats();
+  } else if (roundWinner === 'computer win') {
+    statsHeader.textContent = 'Round lost!';
+    statsPara.textContent = `Computer choice: ${computerChoice}.`;
+    computerScore++;
+    roundCounter++;
+    updateStats();
+  } else {
+    statsHeader.textContent = 'Draw!'
+    statsPara.textContent = `Computer choice: ${computerChoice}.`;
+    roundCounter++;
+    updateStats();
   }
+  statsContainer.appendChild(statsHeader);
+  statsContainer.appendChild(statsPara);
 }
+
+btnRock.addEventListener("click", () => {playRound('rock');});
+btnPaper.addEventListener("click", () => {playRound('paper')});
+btnSciss.addEventListener("click", () => {playRound('scissors')});
